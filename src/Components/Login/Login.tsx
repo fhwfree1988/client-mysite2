6b9a941b5d89555f3a8ddd54a2fc15a83cc5212d
Foad <FuadHr>
2022-01-08 15:58:29 +0330

    change

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -2,6 +2,10 @@ import React, {FC, useState} from 'react';
 import './Login.css';
 import PropTypes from 'prop-types';
 
+//https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
+//https://www.bezkoder.com/spring-boot-react-jwt-auth/
+//https://snyk.io/blog/10-react-security-best-practices/
+//https://developer.okta.com/blog/2019/03/06/simple-user-authentication-in-react
 async function loginUser(credentials: any) {
     return fetch('http://localhost:8080/login', {
         method: 'POST',
