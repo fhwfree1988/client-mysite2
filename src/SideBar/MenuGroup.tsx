e927dd8be3521e0ad6e8c035c8393fb3de241df4
Foad <FuadHr>
2022-01-18 18:36:18 +0330

    change

--- src/SideBar/MenuGroup.tsx
+++ src/SideBar/MenuGroup.tsx
@@ -9,14 +9,18 @@ interface groupType {
 }
 
 const MenuGroup /*:FC<groupType>*/ = (props:any) => {
+    const [isOpen,setIsOpen] = useState(false);
+    const onclick=()=>{
+        setIsOpen(!isOpen);
+    };
     //debugger;
     return (
         <div className="MenuGroup">
             {/*{props.group.groupName}*/}
-            <i className={`MenuGroupIcon + ${props.group.css}`}></i>
+            <i className={`MenuGroupIcon + ${props.group.css}`} onClick={onclick}></i>
             <div>
                 {props.group.menuitem.map((item:any) =>(
-                    <MenuItem key={item.menuId} item={item}></MenuItem>
+                    <MenuItem key={item.menuId} item={item} isOpen={isOpen}></MenuItem>
                 ))}
             </div>
         </div>
--- src/SideBar/MenuItem.tsx
+++ src/SideBar/MenuItem.tsx
@@ -1,6 +1,19 @@
+import {useCallback, useEffect, useState} from "react";
+
 const MenuItem = (props:any)=>{
+    const [isOpen,setIsOpen] = useState(props.isOpen);
+    let class_Name = "MenuItem_hide";
+    useCallback((isOpen)=>{
+        setIsOpen(props.isOpen);
+        if(isOpen)
+            class_Name ="MenuItem_show";
+    },[isOpen]);
+   /* useEffect((props.isOpen) => {
+
+    },[isOpen]);*/
     return(
-        <div className="MenuItem">{props.item.menuTitle}</div>
+
+        <div className={`MenuItem ${class_Name}`}>{props.item.menuTitle}</div>
     )
 }
 export default MenuItem;
