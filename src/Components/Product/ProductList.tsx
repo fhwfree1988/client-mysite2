0e6ba039408c6fa098144fd2fb92e0fe7bd9ac15
Foad <FuadHr>
2022-07-25 19:02:44 +0430

    change

--- src/Components/Product/ProductList.tsx
+++ src/Components/Product/ProductList.tsx
@@ -1,10 +1,17 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import "./Resources/css/Product.css";
 import Product from "./Product";
 
 const ProductList =()=>{
     const[pageState,setPageState] = useState([0]);
 
+    /*client({method: 'GET', path: '/api/employees'}).done(response => {
+        this.setState({employees: response.entity._embedded.employees});
+    });*/
+
+    useEffect(()=>{
+        console.log("Init ProductList")
+    },[]);
     return(
         <div className="productlist-container">
             <Product name={"Hi"}></Product>
