4f8f09d8c6c76dedaa552412ff58493d27410f84
Foad <FuadHr>
2022-07-27 18:20:35 +0430

    change

--- src/Components/Product/ProductList.tsx
+++ src/Components/Product/ProductList.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import "./Resources/css/Product.css";
 import Product from "./Product";
 import ProductService from "./ProductService";
+import ProductType from "./ProductType";
 
 const ProductList =()=>{
     const[pageState,setPageState] = useState([0]);
@@ -25,13 +26,16 @@ const ProductList =()=>{
         });
     },[]);
 
+    // @ts-ignore
     return(
 
         <div className="productlist-container">
             {/*{Items}*/}
+           {/* {itemProducts &&
+            <div>{itemProducts.products.map((item: ProductType) => <div><Product name={item.productName}/></div>)}</div>}*/}
             {itemProducts.products.map(
-                item <any> =>(
-                    <Product name={item.name}/>
+                (item:ProductType) =>(
+                    <Product name={item.productName}/>
                 )
             )
             }
---
+++ src/Components/Product/ProductType.tsx
@@ -0,0 +1,4 @@
+export default interface ProductType{
+    id: number,
+    productName : string
+}
