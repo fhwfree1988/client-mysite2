b958a793f3effe54928ec405d646786ce0789434
Foad <FuadHr>
2022-07-27 18:01:34 +0430

    change

--- src/Components/Product/ProductList.tsx
+++ src/Components/Product/ProductList.tsx
@@ -6,30 +6,34 @@ import ProductService from "./ProductService";
 const ProductList =()=>{
     const[pageState,setPageState] = useState([0]);
     const[itemProducts,setItemProductsState] = useState({products:[]});
-
+    let Items = "";
     /*client({method: 'GET', path: '/api/employees'}).done(response => {
         this.setState({employees: response.entity._embedded.employees});
     });*/
-    ProductService.getProducts('P').then((response) => {
+    /*ProductService.getProducts('P').then((response) => {
         console.log(response.data);
-        setItemProductsState({ products: response.data})
-
-    });
+        //setItemProductsState({ products: response.data})
+        itemProducts.products =response.data;
+    });*/
 
     useEffect(()=>{
         console.log("Init ProductList")
-
-    },[itemProducts]);
+        ProductService.getProducts('P').then((response) => {
+            console.log(response.data);
+            setItemProductsState({ products: response.data})
+            //itemProducts.products =response.data;
+        });
+    },[]);
 
     return(
 
         <div className="productlist-container">
-
+            {/*{Items}*/}
             {itemProducts.products.map(
-                (product: { name: any; }) =>
-
-                    <div><Product name={product.name}/></div>
+                item <any> =>(
+                    <Product name={item.name}/>
                 )
+            )
             }
             {/*<Product name={"Hi"}></Product>*/}
         </div>
